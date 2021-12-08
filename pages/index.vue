01e72256aa8089ac901824071d3e5aa009b2f7bb
Isaac <isaac.cortez1294@hotmail.com>
2021-12-08 17:03:15 -0600

    Subiendo archivos

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" style="background-color: #563d7c">
+      <b-navbar-brand>
+        <img src="../assets/img/front-end-logo.png" alt="" />
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item active href="#">Home</b-nav-item>
+          <b-nav-item href="#">About</b-nav-item>
+          <b-nav-item href="#">Works</b-nav-item>
+          <b-nav-item href="#">Contact</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
