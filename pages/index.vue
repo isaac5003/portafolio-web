afbfe3d08659d06f8d4315345733842ffbaf4633
Isaac <isaac.cortez1294@hotmail.com>
2021-12-08 19:36:03 -0600

    logo2

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar toggleable="lg" type="dark" style="background-color: #563d7c">
       <b-navbar-brand>
-        <img src="../assets/img/prueba16.png" alt="" />
+        <img src="../assets/img/logo2.png" alt="" />
       </b-navbar-brand>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
